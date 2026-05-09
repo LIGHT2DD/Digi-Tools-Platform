3a3f8e5528dc5915b46ae5bdc028fb095d197a39
Sabber26 <rahmansabber84@gmail.com>
2026-05-09 23:25:30 +0600

    Added NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
-
-
   return (
     <>
-      
+    <header className='container mx-auto max-w-400 px-50'> <NavBar/> </header>
+  
     </>
   )
 }
---
+++ src/Components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,13 @@
+
+const HeroBanner = () => {
+    return (
+       <section className="">
+        <div className="">
+            <div className="left-part"></div>
+            <div className="right-part"></div>
+        </div>
+       </section>
+    );
+};
+
+export default HeroBanner;
